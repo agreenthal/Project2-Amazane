ecaff60ceae5560ef158b8379377c97f4fde6a63
Adam Greenthal <adamgreenthal@gmail.com>
2020-04-22 13:27:41 -0700

    ok

--- server.js
+++ server.js
@@ -36,16 +36,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //Global variables
-app.use(function (req,res, next) {
+app.use(function (req, res, next) {
   res.locals.login = req.isAuthenticated();
-  if(req.user){
+  if (req.user) {
     res.locals.globalUsername = req.user.dataValues.nickname;
   }
   next();
-  });
+});
 
 // Routes
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use('/auth', require('./routes/auth-routes'));
 app.use('/profile', require('./routes/profile-routes'));
 app.use('/api', require('./routes/api-routes'));
@@ -55,8 +55,8 @@ app.use('/', require('./routes/html-routes.js'));
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 const PORT = process.env.PORT || 3000;
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });
